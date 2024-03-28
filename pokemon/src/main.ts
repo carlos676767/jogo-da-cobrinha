1da8836c40dcb62a71f15b15b676ffcfa11f52be
carlos676767 <carlitobr730@gmail.com>
2024-03-27 17:36:10 -0700

    imagem add

--- pokemon/src/main.ts
+++ pokemon/src/main.ts
@@ -1,6 +1,6 @@
 const pokedex = async () => {
   try {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu", {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/?offset=1&limit=1302", {
       method: "GET"
     })
     const data = await response.json()
