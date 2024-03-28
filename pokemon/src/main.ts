ef9245290cccf57a7feadfcdc1c710538fd40650
carlos676767 <carlitobr730@gmail.com>
2024-03-27 17:21:58 -0700

    api retornando os dados

--- pokemon/src/main.ts
+++ pokemon/src/main.ts
@@ -1,1 +1,12 @@
-
+const pokedex = async () => {
+  try {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu", {
+      method: "GET"
+    })
+    const data = await response.json()
+    console.log(data)
+  } catch (erro) {
+    console.error(erro)
+  }
+} 
+pokedex()
